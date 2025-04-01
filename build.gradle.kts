aa785d0033ae09309d1d0b9b3f0d5ee8b29f735d
Ryan Sheridan <0xryann@protonmail.com>
2025-04-01 22:09:03 +0100

    build changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 subprojects {
